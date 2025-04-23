5e7a7ca4d4532ebdc017739cfcfaed8380b8ee5b
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-04-23 09:48:34 -0500

    add local-storage-manager

--- src/js/modules/local-storage-manager.js
+++ src/js/modules/local-storage-manager.js
@@ -1,1 +1,12 @@
 "use strict";
+export function readStorageEntry(entryName) {
+    localStorage.getItem(entryName)
+}
+
+export function writeStorageEntry(entryName, value) {
+    localStorage.setItem(entryName, value)
+}
+
+export function removeStorageEntry(entryName) {
+    localStorage.removeItem(entryName)
+}
