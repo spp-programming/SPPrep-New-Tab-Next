2a3872551ce73dbd0d760008be5d9a4c7b2a3c15
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-04-23 09:48:42 -0500

    add migrations stub

---
+++ src/js/modules/migrations.js
@@ -0,0 +1,4 @@
+import { readStorageEntry, writeStorageEntry, removeStorageEntry } from "./local-storage-manager.js"
+export function runMigrations() {
+    console.log("Running migrations for localStorage")
+}
