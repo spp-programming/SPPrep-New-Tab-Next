d18daf8c0b5f913109843cd060e6d5fe83ce5ecf
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-05-03 22:48:45 -0400

    settings page fixes

--- src/js/options.js
+++ src/js/options.js
@@ -7,12 +7,13 @@ handleFakeLinks()
 function clearExtensionData() {
     localStorage.clear()
     console.log("cleared localStorage!")
-    location.href = "options.html#extension-data-cleared"
+    location.href = "#extension-data-cleared"
     location.reload()
 }
 
 if (location.hash === "#extension-data-cleared") {
     extensionDataClearedAlert.hidden = false
+    extensionDataClearedAlert.scrollIntoView({ behavior: "smooth" })
     history.replaceState(null, "", window.location.pathname)
 }
 
