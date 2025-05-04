b1709a2065deb603013f0ba33a610012c2df5c80
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-05-04 16:54:36 -0400

    add send feedback button

--- src/js/about.js
+++ src/js/about.js
@@ -1,3 +1,7 @@
 "use strict";
 import { handleFakeLinks } from "./modules/fake-links.js"
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerElement => new bootstrap.Tooltip(tooltipTriggerElement))
+
 handleFakeLinks()
--- src/js/options.js
+++ src/js/options.js
@@ -2,6 +2,9 @@
 import { clearEverythingModalButton, extensionDataClearedAlert } from "./modules/options-constants.js"
 import { handleFakeLinks } from "./modules/fake-links.js"
 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerElement => new bootstrap.Tooltip(tooltipTriggerElement))
+
 handleFakeLinks()
 
 function clearExtensionData() {
