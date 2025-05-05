d47ed18cad495ae5c61161cab0230052cc79cc74
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-05-04 20:06:45 -0400

    rotate api key

--- src/js/modules/global-constants.js
+++ src/js/modules/global-constants.js
@@ -1,7 +1,7 @@
 "use strict";
 export const primaryTimeZone = "America/New_York"
 export const currentTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-export const calendarApiKey = "AIzaSyCOQ3EYDqe7-ypGvnNb1NZQN6Ib2eb7LG0"
+export const calendarApiKey = "AIzaSyD3GwU8oQO5OEgUO6DbwgdsaO8SShJYkQ8"
 export const calendarApiId = "144grand@gmail.com"
 export const clockElement = document.getElementById("clock")
 export const letterDayElement = document.getElementById("letter-day")
