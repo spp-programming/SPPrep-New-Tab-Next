a09774ede73f4a607d0fde3072768996b8a8c4e0
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-05-20 19:59:11 -0400

    change secret settings password

--- src/js/modules/popup-constants.js
+++ src/js/modules/popup-constants.js
@@ -1,7 +1,7 @@
 "use strict";
 export const titleTextElement = document.getElementById("title-text")
 export const codeEntryInput = document.getElementById("code-entry-input")
-export const codeEntryActualCode = "upupdowndownleftrightleftrightbastart" // This is the secret settings password. If you went out of your way to find this, you deserve it!
+export const codeEntryActualCode = "xyzzy" // This is the secret settings password. If you went out of your way to find this, you deserve it!
 export const codeEntryAlert = document.getElementById("code-entry-alert")
 export const codeConfirmButton = document.getElementById("code-confirm-button")
 export const codeEntryArea = document.getElementById("code-entry-area")
