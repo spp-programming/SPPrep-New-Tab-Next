f7cbed125829b50501208232067c6f44ae8e0a67
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-07-05 13:10:36 -0400

    also clear out localstorage when clearing extension data

--- src/js/clear-data.js
+++ src/js/clear-data.js
@@ -5,6 +5,8 @@ import { handleFakeLinks } from "./modules/fake-links.js"
 async function clearExtensionData() {
     await chrome.storage.local.clear()
     console.log("cleared extension storage!")
+    await localStorage.clear() // Clearing out localStorage can fix issues with migrations, so that's why we do this.
+    console.log("cleared localStorage!")
     chrome.runtime.reload()
 }
 
