e5f3837222d8bbb815ca8ad671b27f5cbce12872
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-07-21 19:43:48 -0400

    initialize tooltips on the clear data page

--- src/js/clear-data.js
+++ src/js/clear-data.js
@@ -2,6 +2,9 @@
 import { clearEverythingModalButton } from "./modules/clear-data-constants.js"
 import { handleFakeLinks } from "./modules/fake-links.js"
 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerElement => new bootstrap.Tooltip(tooltipTriggerElement))
+
 async function clearExtensionData() {
     await chrome.storage.local.clear()
     console.log("cleared extension storage!")
