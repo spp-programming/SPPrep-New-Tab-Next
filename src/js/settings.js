454ce47a548e9670cca83431c3bf01d77ff59906
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-07-29 23:15:17 -0400

    add icon uploader to custom link settings

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,7 +1,7 @@
 "use strict"
 import { handleFakeLinks } from "./modules/fake-links.js"
 import { runMigrations } from "./modules/migrations.js"
-import { settingsCustomLink1NameInput, settingsCustomLink1Switch, settingsCustomLink1URLInput, settingsCustomLink2NameInput, settingsCustomLink2Switch, settingsCustomLink2URLInput, settingsCustomLink3NameInput, settingsCustomLink3Switch, settingsCustomLink3URLInput, settingsCustomLinkCards, settingsEnableCustomLinksSwitch } from "./modules/settings-constants.js"
+import { settingsCustomLink1IconUploader, settingsCustomLink1IconUploaderReal, settingsCustomLink1NameInput, settingsCustomLink1Switch, settingsCustomLink1URLInput, settingsCustomLink2IconUploader, settingsCustomLink2IconUploaderReal, settingsCustomLink2NameInput, settingsCustomLink2Switch, settingsCustomLink2URLInput, settingsCustomLink3IconUploader, settingsCustomLink3IconUploaderReal, settingsCustomLink3NameInput, settingsCustomLink3Switch, settingsCustomLink3URLInput, settingsCustomLinkCards, settingsEnableCustomLinksSwitch } from "./modules/settings-constants.js"
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerElement => new bootstrap.Tooltip(tooltipTriggerElement))
@@ -18,31 +18,49 @@ function isValidURL(string) {
 }
 
 function enableCustomLink1CardContent() {
+    settingsCustomLink1IconUploader.classList.remove("disabled")
+    settingsCustomLink1IconUploader.removeAttribute("aria-disabled")
+    settingsCustomLink1IconUploader.removeAttribute("tabindex")
     settingsCustomLink1NameInput.disabled = false
     settingsCustomLink1URLInput.disabled = false
 }
 
 function disableCustomLink1CardContent() {
+    settingsCustomLink1IconUploader.classList.add("disabled")
+    settingsCustomLink1IconUploader.setAttribute("aria-disabled", "true")
+    settingsCustomLink1IconUploader.setAttribute("tabindex", "-1")
     settingsCustomLink1NameInput.disabled = true
     settingsCustomLink1URLInput.disabled = true
 }
 
 function enableCustomLink2CardContent() {
+    settingsCustomLink2IconUploader.classList.remove("disabled")
+    settingsCustomLink2IconUploader.removeAttribute("aria-disabled")
+    settingsCustomLink2IconUploader.removeAttribute("tabindex")
     settingsCustomLink2NameInput.disabled = false
     settingsCustomLink2URLInput.disabled = false
 }
 
 function disableCustomLink2CardContent() {
+    settingsCustomLink2IconUploader.classList.add("disabled")
+    settingsCustomLink2IconUploader.setAttribute("aria-disabled", "true")
+    settingsCustomLink2IconUploader.setAttribute("tabindex", "-1")
     settingsCustomLink2NameInput.disabled = true
     settingsCustomLink2URLInput.disabled = true
 }
 
 function enableCustomLink3CardContent() {
+    settingsCustomLink3IconUploader.classList.remove("disabled")
+    settingsCustomLink3IconUploader.removeAttribute("aria-disabled")
+    settingsCustomLink3IconUploader.removeAttribute("tabindex")
     settingsCustomLink3NameInput.disabled = false
     settingsCustomLink3URLInput.disabled = false
 }
 
 function disableCustomLink3CardContent() {
+    settingsCustomLink3IconUploader.classList.add("disabled")
+    settingsCustomLink3IconUploader.setAttribute("aria-disabled", "true")
+    settingsCustomLink3IconUploader.setAttribute("tabindex", "-1")
     settingsCustomLink3NameInput.disabled = true
     settingsCustomLink3URLInput.disabled = true
 }
@@ -73,6 +91,30 @@ settingsEnableCustomLinksSwitch.addEventListener("change", () => {
     if (settingsEnableCustomLinksSwitch.checked) {
         settingsCustomLinkCards.hidden = false
     }
+})
+
+settingsCustomLink1IconUploader.addEventListener("click", () => {
+    if (settingsCustomLink1IconUploader.classList.contains("disabled")) {
+        console.error("Icon uploader clicked while being disabled, this should not be happening!")
+        return
+    }
+    settingsCustomLink1IconUploaderReal.click()
+})
+
+settingsCustomLink2IconUploader.addEventListener("click", () => {
+    if (settingsCustomLink2IconUploader.classList.contains("disabled")) {
+        console.error("Icon uploader clicked while being disabled, this should not be happening!")
+        return
+    }
+    settingsCustomLink2IconUploaderReal.click()
+})
+
+settingsCustomLink3IconUploader.addEventListener("click", () => {
+    if (settingsCustomLink3IconUploader.classList.contains("disabled")) {
+        console.error("Icon uploader clicked while being disabled, this should not be happening!")
+        return
+    }
+    settingsCustomLink3IconUploaderReal.click()
 })
 
 async function loadStuff() {
