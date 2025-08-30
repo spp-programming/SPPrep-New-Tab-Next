84acace1a8e10b9740e2c6db70959361f24425b7
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-08-30 14:19:09 -0400

    add validation for custom link icon size

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,7 +1,7 @@
 "use strict"
 import { handleFakeLinks } from "./modules/fake-links.js"
 import { runMigrations } from "./modules/migrations.js"
-import { settingsCustomLink1IconUploader, settingsCustomLink1IconUploaderReal, settingsCustomLink1NameInput, settingsCustomLink1Switch, settingsCustomLink1URLInput, settingsCustomLink2IconUploader, settingsCustomLink2IconUploaderReal, settingsCustomLink2NameInput, settingsCustomLink2Switch, settingsCustomLink2URLInput, settingsCustomLink3IconUploader, settingsCustomLink3IconUploaderReal, settingsCustomLink3NameInput, settingsCustomLink3Switch, settingsCustomLink3URLInput, settingsCustomLinkCards, settingsEnableCustomLinksSwitch, settingsHideSchoolCalendarSwitch, settingsSaveButton } from "./modules/settings-constants.js"
+import { settingsCustomLink1IconUploader, settingsCustomLink1IconUploaderAlertWrapper, settingsCustomLink1IconUploaderReal, settingsCustomLink1NameInput, settingsCustomLink1Switch, settingsCustomLink1URLInput, settingsCustomLink2IconUploader, settingsCustomLink2IconUploaderAlertWrapper, settingsCustomLink2IconUploaderReal, settingsCustomLink2NameInput, settingsCustomLink2Switch, settingsCustomLink2URLInput, settingsCustomLink3IconUploader, settingsCustomLink3IconUploaderAlertWrapper, settingsCustomLink3IconUploaderReal, settingsCustomLink3NameInput, settingsCustomLink3Switch, settingsCustomLink3URLInput, settingsCustomLinkCards, settingsEnableCustomLinksSwitch, settingsHideSchoolCalendarSwitch, settingsSaveButton } from "./modules/settings-constants.js"
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerElement => new bootstrap.Tooltip(tooltipTriggerElement))
@@ -117,9 +117,121 @@ settingsCustomLink3IconUploader.addEventListener("click", () => {
     settingsCustomLink3IconUploaderReal.click()
 })
 
+settingsCustomLink1IconUploaderReal.addEventListener("change", async () => {
+    settingsSaveButton.disabled = true
+    settingsCustomLink1Switch.disabled = true
+    settingsCustomLink1IconUploader.classList.add("disabled")
+    settingsCustomLink1IconUploader.setAttribute("aria-disabled", "true")
+    settingsCustomLink1IconUploader.setAttribute("tabindex", "-1")
+    let iconURL
+    if (validateCustomLink1IconUploaderFileList() === true) {
+        iconURL = await constructCustomLinkIconURL(settingsCustomLink1IconUploaderReal.files[0])
+        settingsCustomLink1IconUploader.querySelector("img").src = iconURL
+    }
+    settingsCustomLink1IconUploaderReal.value = ""
+    settingsSaveButton.disabled = false
+    settingsCustomLink1Switch.disabled = false
+    settingsCustomLink1IconUploader.classList.remove("disabled")
+    settingsCustomLink1IconUploader.removeAttribute("aria-disabled")
+    settingsCustomLink1IconUploader.removeAttribute("tabindex")
+})
+
+settingsCustomLink2IconUploaderReal.addEventListener("change", async () => {
+    settingsSaveButton.disabled = true
+    settingsCustomLink2Switch.disabled = true
+    settingsCustomLink2IconUploader.classList.add("disabled")
+    settingsCustomLink2IconUploader.setAttribute("aria-disabled", "true")
+    settingsCustomLink2IconUploader.setAttribute("tabindex", "-1")
+    let iconURL
+    if (validateCustomLink2IconUploaderFileList() === true) {
+        iconURL = await constructCustomLinkIconURL(settingsCustomLink2IconUploaderReal.files[0])
+        settingsCustomLink2IconUploader.querySelector("img").src = iconURL
+    }
+    settingsCustomLink2IconUploaderReal.value = ""
+    settingsSaveButton.disabled = false
+    settingsCustomLink2Switch.disabled = false
+    settingsCustomLink2IconUploader.classList.remove("disabled")
+    settingsCustomLink2IconUploader.removeAttribute("aria-disabled")
+    settingsCustomLink2IconUploader.removeAttribute("tabindex")
+})
+
+settingsCustomLink3IconUploaderReal.addEventListener("change", async () => {
+    settingsSaveButton.disabled = true
+    settingsCustomLink3Switch.disabled = true
+    settingsCustomLink3IconUploader.classList.add("disabled")
+    settingsCustomLink3IconUploader.setAttribute("aria-disabled", "true")
+    settingsCustomLink3IconUploader.setAttribute("tabindex", "-1")
+    let iconURL
+    if (validateCustomLink3IconUploaderFileList() === true) {
+        iconURL = await constructCustomLinkIconURL(settingsCustomLink3IconUploaderReal.files[0])
+        settingsCustomLink3IconUploader.querySelector("img").src = iconURL
+    }
+    settingsCustomLink3IconUploaderReal.value = ""
+    settingsSaveButton.disabled = false
+    settingsCustomLink3Switch.disabled = false
+    settingsCustomLink3IconUploader.classList.remove("disabled")
+    settingsCustomLink3IconUploader.removeAttribute("aria-disabled")
+    settingsCustomLink3IconUploader.removeAttribute("tabindex")
+})
+
 settingsSaveButton.addEventListener("click", () => {
     saveSettings()
 })
+
+function validateCustomLink1IconUploaderFileList() {
+    settingsCustomLink1IconUploaderAlertWrapper.innerHTML = ""
+    if (settingsCustomLink1IconUploaderReal.files.length === 0) {
+        console.log("Custom link (1) icon validator: No files selected.")
+        return false
+    }
+    if (settingsCustomLink1IconUploaderReal.files[0].size > 512000) {
+        console.error(`Custom link (1) icon validator: File "${settingsCustomLink1IconUploaderReal.files[0].name}" is larger than 512000 bytes!`)
+        settingsCustomLink1IconUploaderAlertWrapper.innerHTML = `<div class="alert alert-danger alert-dismissible fade show mt-3" role="alert"><div><i class="bi bi-exclamation-triangle" aria-hidden="true"></i> <span>File is larger than 512 kilobytes! Reduce the file size of your image before trying again.</span></div><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`
+        return false
+    }
+    return true
+}
+
+function validateCustomLink2IconUploaderFileList() {
+    settingsCustomLink2IconUploaderAlertWrapper.innerHTML = ""
+    if (settingsCustomLink2IconUploaderReal.files.length === 0) {
+        console.log("Custom link (2) icon validator: No files selected.")
+        return false
+    }
+    if (settingsCustomLink2IconUploaderReal.files[0].size > 512000) {
+        console.error(`Custom link (2) icon validator: File "${settingsCustomLink2IconUploaderReal.files[0].name}" is larger than 512000 bytes!`)
+        settingsCustomLink2IconUploaderAlertWrapper.innerHTML = `<div class="alert alert-danger alert-dismissible fade show mt-3" role="alert"><div><i class="bi bi-exclamation-triangle" aria-hidden="true"></i> <span>File is larger than 512 kilobytes! Reduce the file size of your image before trying again.</span></div><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`
+        return false
+    }
+    return true
+}
+
+function validateCustomLink3IconUploaderFileList() {
+    settingsCustomLink3IconUploaderAlertWrapper.innerHTML = ""
+    if (settingsCustomLink3IconUploaderReal.files.length === 0) {
+        console.log("Custom link (3) icon validator: No files selected.")
+        return false
+    }
+    if (settingsCustomLink3IconUploaderReal.files[0].size > 512000) {
+        console.error(`Custom link (3) icon validator: File "${settingsCustomLink3IconUploaderReal.files[0].name}" is larger than 512000 bytes!`)
+        settingsCustomLink3IconUploaderAlertWrapper.innerHTML = `<div class="alert alert-danger alert-dismissible fade show mt-3" role="alert"><div><i class="bi bi-exclamation-triangle" aria-hidden="true"></i> <span>File is larger than 512 kilobytes! Reduce the file size of your image before trying again.</span></div><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`
+        return false
+    }
+    return true
+}
+
+async function constructCustomLinkIconURL(file) {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader()
+        fileReader.addEventListener("load", () => {
+            resolve(fileReader.result)
+        })
+        fileReader.addEventListener("error", () => {
+            reject()
+        })
+        fileReader.readAsDataURL(file)
+    })
+}
 
 async function loadSettings() {
     const storedHideSchoolCalendarSelection = (await chrome.storage.local.get(["settings_hideSchoolCalendarSelection"]))["settings_hideSchoolCalendarSelection"]
