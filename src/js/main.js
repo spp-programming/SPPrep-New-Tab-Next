9f410ffb7c384e36c729a5987af00cef176a6706
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-08-31 23:48:46 -0400

    finish custom icons feature (fixes #5)

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,6 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerElement => new bootstrap.Popover(popoverTriggerElement))
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerElement => new bootstrap.Tooltip(tooltipTriggerElement))
-
 function setPopoverText(triggerElement, content) {
     const popover = bootstrap.Popover.getInstance(triggerElement)
     popover._config.content = content
@@ -14,7 +11,7 @@ function setPopoverText(triggerElement, content) {
 import { getTodaysEvents, dateString, getCurrentDateString } from "./modules/calendar-api.js"
 import { getLetterDay } from "./modules/letter-day-extractor.js"
 import { updateTime } from "./modules/clock-manager.js"
-import { clockElement, letterDayElement, emblemElement, errorToast, currentTimeZone, errorToastContent, powerSchoolButton, powerSchoolTeacherURL, powerSchoolStudentURL, backgroundBliss, backgroundOsxLeopard, backgroundOsxTiger, backgroundOsxLion, backgroundOsxYosemite, backgroundMscBuilding, backgroundSnow, backgroundSnowLowQuality, backgroundStaffStaring, backgroundStreetView, backgroundStreetViewBetter, backgroundRainbow, validFonts, schoolCalendarButton } from "./modules/global-constants.js"
+import { clockElement, letterDayElement, emblemElement, errorToast, currentTimeZone, errorToastContent, powerSchoolButton, powerSchoolTeacherURL, powerSchoolStudentURL, backgroundBliss, backgroundOsxLeopard, backgroundOsxTiger, backgroundOsxLion, backgroundOsxYosemite, backgroundMscBuilding, backgroundSnow, backgroundSnowLowQuality, backgroundStaffStaring, backgroundStreetView, backgroundStreetViewBetter, backgroundRainbow, validFonts, schoolCalendarButton, customLinkTemplate, buttonContainer } from "./modules/global-constants.js"
 import { openPasscodeModal } from "./modules/passcode-modal.js"
 import { handleFakeLinks } from "./modules/fake-links.js"
 import { runMigrations } from "./modules/migrations.js"
@@ -83,19 +80,82 @@ async function applyInternalConfigModeChanges() {
 }
 
 async function loadAllSettings() {
-    const storedHideSchoolCalendarSelection = (await chrome.storage.local.get(["settings_hideSchoolCalendarSelection"]))["settings_hideSchoolCalendarSelection"]
-    const storedBackgroundSelection = (await chrome.storage.local.get(["secretSettings_backgroundSelection"]))["secretSettings_backgroundSelection"]
-    const storedCustomBackground = (await chrome.storage.local.get(["secretSettings_customBackground"]))["secretSettings_customBackground"]
-    const storedFontSelection = (await chrome.storage.local.get(["secretSettings_fontSelection"]))["secretSettings_fontSelection"]
-    const storedGradientSelection = (await chrome.storage.local.get(["secretSettings_gradientSelection"]))["secretSettings_gradientSelection"]
-    if (storedHideSchoolCalendarSelection === true) {
+    const storedSettingsHideSchoolCalendarSelection = (await chrome.storage.local.get(["settings_hideSchoolCalendarSelection"]))["settings_hideSchoolCalendarSelection"]
+    const storedSettingsEnableCustomLinksSelection = (await chrome.storage.local.get(["settings_enableCustomLinksSelection"]))["settings_enableCustomLinksSelection"]
+    const storedSettingsCustomLink1Enabled = (await chrome.storage.local.get())["settings_customLink1Enabled"]
+    const storedSettingsCustomLink2Enabled = (await chrome.storage.local.get())["settings_customLink2Enabled"]
+    const storedSettingsCustomLink3Enabled = (await chrome.storage.local.get())["settings_customLink3Enabled"]
+    const storedSettingsCustomLink1IconURL = (await chrome.storage.local.get())["settings_customLink1IconURL"]
+    const storedSettingsCustomLink2IconURL = (await chrome.storage.local.get())["settings_customLink2IconURL"]
+    const storedSettingsCustomLink3IconURL = (await chrome.storage.local.get())["settings_customLink3IconURL"]
+    const storedSettingsCustomLink1Name = (await chrome.storage.local.get())["settings_customLink1Name"]
+    const storedSettingsCustomLink2Name = (await chrome.storage.local.get())["settings_customLink2Name"]
+    const storedSettingsCustomLink3Name = (await chrome.storage.local.get())["settings_customLink3Name"]
+    const storedSettingsCustomLink1URL = (await chrome.storage.local.get())["settings_customLink1URL"]
+    const storedSettingsCustomLink2URL = (await chrome.storage.local.get())["settings_customLink2URL"]
+    const storedSettingsCustomLink3URL = (await chrome.storage.local.get())["settings_customLink3URL"]
+    const storedSecretSettingsBackgroundSelection = (await chrome.storage.local.get(["secretSettings_backgroundSelection"]))["secretSettings_backgroundSelection"]
+    const storedSecretSettingsCustomBackground = (await chrome.storage.local.get(["secretSettings_customBackground"]))["secretSettings_customBackground"]
+    const storedSecretSettingsFontSelection = (await chrome.storage.local.get(["secretSettings_fontSelection"]))["secretSettings_fontSelection"]
+    const storedSecretSettingsGradientSelection = (await chrome.storage.local.get(["secretSettings_gradientSelection"]))["secretSettings_gradientSelection"]
+    if (storedSettingsHideSchoolCalendarSelection === true) {
         schoolCalendarButton.hidden = true
     }
-    switch (storedBackgroundSelection) {
+    if (storedSettingsEnableCustomLinksSelection === true) {
+        if (storedSettingsCustomLink1Enabled === true && storedSettingsCustomLink1URL !== undefined) {
+            let name = "Custom link #1"
+            let iconURL = "./img/icons/globe2.svg"
+            const url = storedSettingsCustomLink1URL
+            if (storedSettingsCustomLink1Name !== undefined && storedSettingsCustomLink1Name.trim() !== "") {
+                name = storedSettingsCustomLink1Name.trim()
+            }
+            if (storedSettingsCustomLink1IconURL !== undefined) {
+                iconURL = storedSettingsCustomLink1IconURL
+            }
+            const content = customLinkTemplate.content.firstElementChild.cloneNode(true)
+            content.title = name
+            content.href = url
+            content.querySelector("img").src = iconURL
+            buttonContainer.appendChild(content)
+        }
+        if (storedSettingsCustomLink2Enabled === true && storedSettingsCustomLink2URL !== undefined) {
+            let name = "Custom link #2"
+            let iconURL = "./img/icons/globe2.svg"
+            const url = storedSettingsCustomLink2URL
+            if (storedSettingsCustomLink2Name !== undefined && storedSettingsCustomLink2Name.trim() !== "") {
+                name = storedSettingsCustomLink2Name.trim()
+            }
+            if (storedSettingsCustomLink2IconURL !== undefined) {
+                iconURL = storedSettingsCustomLink2IconURL
+            }
+            const content = customLinkTemplate.content.firstElementChild.cloneNode(true)
+            content.title = name
+            content.href = url
+            content.querySelector("img").src = iconURL
+            buttonContainer.appendChild(content)
+        }
+        if (storedSettingsCustomLink3Enabled === true && storedSettingsCustomLink3URL !== undefined) {
+            let name = "Custom link #3"
+            let iconURL = "./img/icons/globe2.svg"
+            const url = storedSettingsCustomLink3URL
+            if (storedSettingsCustomLink3Name !== undefined && storedSettingsCustomLink3Name.trim() !== "") {
+                name = storedSettingsCustomLink3Name.trim()
+            }
+            if (storedSettingsCustomLink3IconURL !== undefined) {
+                iconURL = storedSettingsCustomLink3IconURL
+            }
+            const content = customLinkTemplate.content.firstElementChild.cloneNode(true)
+            content.title = name
+            content.href = url
+            content.querySelector("img").src = iconURL
+            buttonContainer.appendChild(content)
+        }
+    }
+    switch (storedSecretSettingsBackgroundSelection) {
         case "custom":
-            if (storedCustomBackground !== undefined) {
+            if (storedSecretSettingsCustomBackground !== undefined) {
                 try {
-                    const backgroundBlob = (await (await fetch(storedCustomBackground)).blob())
+                    const backgroundBlob = (await (await fetch(storedSecretSettingsCustomBackground)).blob())
                     document.documentElement.style.setProperty("--selected-background", `url("${URL.createObjectURL(backgroundBlob)}")`)
                 } catch (error) {
                     // If fetch() fails for whatever reason (for example, on an invalid data URL), just log the error but move on with the rest of the function.
@@ -146,11 +206,11 @@ async function loadAllSettings() {
         default:
             document.documentElement.style.setProperty("--selected-background", `url(".${getSeasonalBackground((new Date()).getMonth(), (new Date()).getDate())}")`)
     }
-    if (validFonts.includes(storedFontSelection)) {
-        document.documentElement.classList.add(`font-${storedFontSelection}`)
+    if (validFonts.includes(storedSecretSettingsFontSelection)) {
+        document.documentElement.classList.add(`font-${storedSecretSettingsFontSelection}`)
     }
-    if (/^#[0-9A-F]{6}$/i.test(storedGradientSelection)) {
-        document.documentElement.style.setProperty("--gradient-color", storedGradientSelection)
+    if (/^#[0-9A-F]{6}$/i.test(storedSecretSettingsGradientSelection)) {
+        document.documentElement.style.setProperty("--gradient-color", storedSecretSettingsGradientSelection)
     }
 }
 
@@ -167,6 +227,8 @@ async function loadStuff() {
     updateTimeHere()
     setInterval(updateTimeHere, 1) // Calling updateTime every 1000 ms causes noticeable lag (many milliseconds) so it is called every millisecond to avoid this problem
     await loadAllSettings()
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerElement => new bootstrap.Tooltip(tooltipTriggerElement))
 }
 
 emblemElement.addEventListener("dblclick", () => {
