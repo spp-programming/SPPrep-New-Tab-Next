12b4b894cd292902370a6b052334adff2e1af814
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-09-05 13:34:00 -0400

    finish changelog (fixes #7)

--- src/js/changelog.js
+++ src/js/changelog.js
@@ -3,5 +3,16 @@ import { handleFakeLinks } from "./modules/fake-links.js"
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerElement => new bootstrap.Tooltip(tooltipTriggerElement))
+const secretItems = document.getElementsByClassName("secret-item")
 
 handleFakeLinks()
+async function handleSecretSettingsVisibility() {
+    const secretSettingsVisible = (await chrome.storage.local.get())["secretSettingsVisible"]
+
+    if (secretSettingsVisible === "true") {
+        Array.from(secretItems).forEach(element => {
+            element.hidden = false
+        })
+    }
+}
+handleSecretSettingsVisibility()
