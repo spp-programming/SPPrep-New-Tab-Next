1a7a2c1fd2244209d9dee683a5676f79597c8b8e
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-09-22 17:07:40 -0400

    use textContent for checking if the clock needs to be updated, because it's slightly faster

--- src/js/modules/clock-manager.js
+++ src/js/modules/clock-manager.js
@@ -6,7 +6,7 @@ export function updateTime(clockElement) {
         .toString()
         .padStart(2, "0")}:${d.getSeconds().toString().padStart(2, "0")}`
     // Although updateTime is called every millisecond, we should only update the DOM when it's needed
-    if (clockElement.innerHTML != currentTime) {
+    if (clockElement.textContent != currentTime) {
         clockElement.innerHTML = currentTime
     }
 }
