f5e2aaeab39095870d24d61103a2e104c36cccd7
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-11-13 10:30:17 -0500

    hotfix for letter day extractor (Fixes #11)

--- src/js/modules/letter-day-extractor.js
+++ src/js/modules/letter-day-extractor.js
@@ -30,54 +30,55 @@ export function getLetterDay(todaysEvents) {
 }
 
 function extractLetterDay(eventName) {
-    // Is there any way to optimize this atrocity?
-    const exactlyMatchesLetterA = ["A", "A ", " A", " A ", "A  ", "  A", "  A  "] 
-    const startsWithLetterA = ["A - Assembly", "A- Assembly", " A -Assembly", "A-Assembly", " A - Assembly", " A- Assembly", " A -Assembly", " A-Assembly", "A (Cycle", "A(Cycle", "A ( Cycle", "A( Cycle", " A (Cycle", " A(Cycle", " A (Cycle", " A(Cycle", " A ( Cycle", " A( Cycle", "A (End of MP", "A(End of MP", "A ( End of MP", "A( End of MP", " A (End of MP", " A(End of MP", " A (End of MP", " A(End of MP", " A ( End of MP", " A( End of MP"]
-    const exactlyMatchesLetterB = ["B", "B ", " B", " B ", "B  ", "  B", "  B  "]
-    const startsWithLetterB = ["B - Assembly", "B- Assembly", " B -Assembly", "B-Assembly", " B - Assembly", " B- Assembly", " B -Assembly", " B-Assembly", "B (Cycle", "B(Cycle", "B ( Cycle", "B( Cycle", " B (Cycle", " B(Cycle", " B (Cycle", " B(Cycle", " B ( Cycle", " B( Cycle", "B (End of MP", "B(End of MP", "B ( End of MP", "B( End of MP", " B (End of MP", " B(End of MP", " B (End of MP", " B(End of MP", " B ( End of MP", " B( End of MP"]
-    const exactlyMatchesLetterC = ["C", "C ", " C", " C ", "C  ", "  C", "  C  "]
-    const startsWithLetterC = ["C - Assembly", "C- Assembly", " C -Assembly", "C-Assembly", " C - Assembly", " C- Assembly", " C -Assembly", " C-Assembly", "C (Cycle", "C(Cycle", "C ( Cycle", "C( Cycle", " C (Cycle", " C(Cycle", " C (Cycle", " C(Cycle", " C ( Cycle", " C( Cycle", "C (End of MP", "C(End of MP", "C ( End of MP", "C( End of MP", " C (End of MP", " C(End of MP", " C (End of MP", " C(End of MP", " C ( End of MP", " C( End of MP"]
-    const exactlyMatchesLetterD = ["D", "D ", " D", " D ", "D  ", "  D", "  D  "]
-    const startsWithLetterD = ["D - Assembly", "D- Assembly", " D -Assembly", "D-Assembly", " D - Assembly", " D- Assembly", " D -Assembly", " D-Assembly", "D (Cycle", "D(Cycle", "D ( Cycle", "D( Cycle", " D (Cycle", " D(Cycle", " D (Cycle", " D(Cycle", " D ( Cycle", " D( Cycle", "D (End of MP", "D(End of MP", "D ( End of MP", "D( End of MP", " D (End of MP", " D(End of MP", " D (End of MP", " D(End of MP", " D ( End of MP", " D( End of MP"]
-    const exactlyMatchesLetterE = ["E", "E ", " E", " E ", "E  ", "  E", "  E  "]
-    const startsWithLetterE = ["E - Assembly", "E- Assembly", " E -Assembly", "E-Assembly", " E - Assembly", " E- Assembly", " E -Assembly", " E-Assembly", "E (Cycle", "E(Cycle", "E ( Cycle", "E( Cycle", " E (Cycle", " E(Cycle", " E (Cycle", " E(Cycle", " E ( Cycle", " E( Cycle", "E (End of MP", "E(End of MP", "E ( End of MP", "E( End of MP", " E (End of MP", " E(End of MP", " E (End of MP", " E(End of MP", " E ( End of MP", " E( End of MP"]
-    const exactlyMatchesLetterF = ["F", "F ", " F", " F ", "F  ", "  F", "  F  "]
-    const startsWithLetterF = ["F - Assembly", "F- Assembly", " F -Assembly", "F-Assembly", " F - Assembly", " F- Assembly", " F -Assembly", " F-Assembly", "F (Cycle", "F(Cycle", "F ( Cycle", "F( Cycle", " F (Cycle", " F(Cycle", " F (Cycle", " F(Cycle", " F ( Cycle", " F( Cycle", "F (End of MP", "F(End of MP", "F ( End of MP", "F( End of MP", " F (End of MP", " F(End of MP", " F (End of MP", " F(End of MP", " F ( End of MP", " F( End of MP"]
-    const exactlyMatchesLetterG = ["G", "G ", " G", " G ", "G  ", "  G", "  G  "]
-    const startsWithLetterG = ["G - Assembly", "G- Assembly", " G -Assembly", "G-Assembly", " G - Assembly", " G- Assembly", " G -Assembly", " G-Assembly", "G (Cycle", "G(Cycle", "G ( Cycle", "G( Cycle", " G (Cycle", " G(Cycle", " G (Cycle", " G(Cycle", " G ( Cycle", " G( Cycle", "G (End of MP", "G(End of MP", "G ( End of MP", "G( End of MP", " G (End of MP", " G(End of MP", " G (End of MP", " G(End of MP", " G ( End of MP", " G( End of MP"]
-    const exactlyMatchesLetterH = ["H", "H ", " H", " H ", "H  ", "  H", "  H  "]
-    const startsWithLetterH = ["H - Assembly", "H- Assembly", " H -Assembly", "H-Assembly", " H - Assembly", " H- Assembly", " H -Assembly", " H-Assembly", "H (Cycle", "H(Cycle", "H ( Cycle", "H( Cycle", " H (Cycle", " H(Cycle", " H (Cycle", " H(Cycle", " H ( Cycle", " H( Cycle", "H (End of MP", "H(End of MP", "H ( End of MP", "H( End of MP", " H (End of MP", " H(End of MP", " H (End of MP", " H(End of MP", " H ( End of MP", " H( End of MP"]
-    if (exactlyMatchesLetterA.includes(eventName)) {
+    const eventNameTrimmed = eventName.trim()
+    // Is there any way to optimize this atrocity? Update: Nvm, I actually did optimize it :)
+    const exactlyMatchesLetterA = ["A"] 
+    const startsWithLetterA = ["A - Assembly", "A- Assembly", "A -Assembly", "A-Assembly", "A (Cycle", "A(Cycle", "A ( Cycle", "A( Cycle", "A (End of MP", "A(End of MP", "A ( End of MP", "A( End of MP"]
+    const exactlyMatchesLetterB = ["B"]
+    const startsWithLetterB = ["B - Assembly", "B- Assembly", "B -Assembly", "B-Assembly", "B (Cycle", "B(Cycle", "B ( Cycle", "B( Cycle", "B (End of MP", "B(End of MP", "B ( End of MP", "B( End of MP"]
+    const exactlyMatchesLetterC = ["C"]
+    const startsWithLetterC = ["C - Assembly", "C- Assembly", "C -Assembly", "C-Assembly", "C (Cycle", "C(Cycle", "C ( Cycle", "C( Cycle", "C (End of MP", "C(End of MP", "C ( End of MP", "C( End of MP"]
+    const exactlyMatchesLetterD = ["D"]
+    const startsWithLetterD = ["D - Assembly", "D- Assembly", "D -Assembly", "D-Assembly", "D (Cycle", "D(Cycle", "D ( Cycle", "D( Cycle", "D (End of MP", "D(End of MP", "D ( End of MP", "D( End of MP"]
+    const exactlyMatchesLetterE = ["E"]
+    const startsWithLetterE = ["E - Assembly", "E- Assembly", "E -Assembly", "E-Assembly", "E (Cycle", "E(Cycle", "E ( Cycle", "E( Cycle", "E (End of MP", "E(End of MP", "E ( End of MP", "E( End of MP"]
+    const exactlyMatchesLetterF = ["F"]
+    const startsWithLetterF = ["F - Assembly", "F- Assembly", "F -Assembly", "F-Assembly", "F (Cycle", "F(Cycle", "F ( Cycle", "F( Cycle", "F (End of MP", "F(End of MP", "F ( End of MP", "F( End of MP"]
+    const exactlyMatchesLetterG = ["G"]
+    const startsWithLetterG = ["G - Assembly", "G- Assembly", "G -Assembly", "G-Assembly", "G (Cycle", "G(Cycle", "G ( Cycle", "G( Cycle", "G (End of MP", "G(End of MP", "G ( End of MP", "G( End of MP"]
+    const exactlyMatchesLetterH = ["H"]
+    const startsWithLetterH = ["H - Assembly", "H- Assembly", "H -Assembly", "H-Assembly", "H (Cycle", "H(Cycle", "H ( Cycle", "H( Cycle", "H (End of MP", "H(End of MP", "H ( End of MP", "H( End of MP"]
+    if (exactlyMatchesLetterA.includes(eventNameTrimmed)) {
         return "A"
-    } else if (startsWithLetterA.some(substr => eventName.startsWith(substr))) {
+    } else if (startsWithLetterA.some(substr => eventNameTrimmed.startsWith(substr))) {
         return "A"
-    } else if (exactlyMatchesLetterB.includes(eventName)) {
+    } else if (exactlyMatchesLetterB.includes(eventNameTrimmed)) {
         return "B"
-    } else if (startsWithLetterB.some(substr => eventName.startsWith(substr))) {
+    } else if (startsWithLetterB.some(substr => eventNameTrimmed.startsWith(substr))) {
         return "B"
-    } else if (exactlyMatchesLetterC.includes(eventName)) {
+    } else if (exactlyMatchesLetterC.includes(eventNameTrimmed)) {
         return "C"
-    } else if (startsWithLetterC.some(substr => eventName.startsWith(substr))) {
+    } else if (startsWithLetterC.some(substr => eventNameTrimmed.startsWith(substr))) {
         return "C"
-    } else if (exactlyMatchesLetterD.includes(eventName)) {
+    } else if (exactlyMatchesLetterD.includes(eventNameTrimmed)) {
         return "D"
-    } else if (startsWithLetterD.some(substr => eventName.startsWith(substr))) {
+    } else if (startsWithLetterD.some(substr => eventNameTrimmed.startsWith(substr))) {
         return "D"
-    } else if (exactlyMatchesLetterE.includes(eventName)) {
+    } else if (exactlyMatchesLetterE.includes(eventNameTrimmed)) {
         return "E"
-    } else if (startsWithLetterE.some(substr => eventName.startsWith(substr))) {
+    } else if (startsWithLetterE.some(substr => eventNameTrimmed.startsWith(substr))) {
         return "E"
-    } else if (exactlyMatchesLetterF.includes(eventName)) {
+    } else if (exactlyMatchesLetterF.includes(eventNameTrimmed)) {
         return "F"
-    } else if (startsWithLetterF.some(substr => eventName.startsWith(substr))) {
+    } else if (startsWithLetterF.some(substr => eventNameTrimmed.startsWith(substr))) {
         return "F"
-    } else if (exactlyMatchesLetterG.includes(eventName)) {
+    } else if (exactlyMatchesLetterG.includes(eventNameTrimmed)) {
         return "G"
-    } else if (startsWithLetterG.some(substr => eventName.startsWith(substr))) {
+    } else if (startsWithLetterG.some(substr => eventNameTrimmed.startsWith(substr))) {
         return "G"
-    } else if (exactlyMatchesLetterH.includes(eventName)) {
+    } else if (exactlyMatchesLetterH.includes(eventNameTrimmed)) {
         return "H"
-    } else if (startsWithLetterH.some(substr => eventName.startsWith(substr))) {
+    } else if (startsWithLetterH.some(substr => eventNameTrimmed.startsWith(substr))) {
         return "H"
     } else {
         // Handle the case where no matching letter day is found
