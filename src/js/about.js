fcaab7b95f7bc176f959de67337e1e68e7543dd6
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-12-12 21:12:52 -0500

    display internal config mode on about page

--- src/js/about.js
+++ src/js/about.js
@@ -1,7 +1,24 @@
 "use strict"
 import { handleFakeLinks } from "./modules/fake-links.js"
+import { internalConfigModeText } from "./modules/about-constants.js"
+import { getInternalConfigMode } from "./modules/config-mode.js"
+
+handleFakeLinks()
+
+switch (await getInternalConfigMode()) {
+    case "student":
+        internalConfigModeText.innerHTML = "Student"
+        internalConfigModeText.title = "You're running the student version of the extension."
+        break;
+    case "staff":
+        internalConfigModeText.innerHTML = "Staff"
+        internalConfigModeText.title = "You're running the staff version of the extension."
+        break;
+    default:
+        internalConfigModeText.innerHTML = "⚠️ Unknown"
+        internalConfigModeText.title = "Unable to determine internal config mode, you may experience issues."
+        break;
+}
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerElement => new bootstrap.Tooltip(tooltipTriggerElement))
-
-handleFakeLinks()
---
+++ src/js/modules/about-constants.js
@@ -0,0 +1,2 @@
+"use strict"
+export const internalConfigModeText = document.getElementById("internal-config-mode-text")
