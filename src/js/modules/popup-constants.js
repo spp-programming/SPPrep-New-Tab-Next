30f32e7b23c06ee054187b2670ed19b69e4ee122
Anon <88592342+anon-123456789@users.noreply.github.com>
2025-12-14 01:15:48 -0500

    removed unnecessary file

--- src/js/modules/popup-constants.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-export const titleTextElement = document.getElementById("title-text")
-export const codeEntryInput = document.getElementById("code-entry-input")
-export const codeEntryActualCode = "xyzzy" // This is the secret settings password. If you went out of your way to find this, you deserve it!
-export const codeEntryAlert = document.getElementById("code-entry-alert")
-export const codeConfirmButton = document.getElementById("code-confirm-button")
-export const codeEntryArea = document.getElementById("code-entry-area")
-export const secretSettingsArea = document.getElementById("secret-settings-area")
