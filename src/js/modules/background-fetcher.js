bfaf0fc715ad1a180898834ce930e67b418d305c
Anon <88592342+anon-123456789@users.noreply.github.com>
2026-08-29 03:56:07 -0400

    use the worker-based background fetcher inside secret settings for custom backgrounds

--- src/js/modules/background-fetcher.js
+++ src/js/modules/background-fetcher.js
@@ -3,10 +3,19 @@ const fetchWorker = new Worker("./js/modules/background-fetcher-worker.js")
 
 /**
  * Gets the blob: URL associated with the stored custom background and its content type. This uses a web worker to avoid blocking the main thread.
- * @returns {Promise<{type: string, url?: string}>}
+ * @returns {Promise<{type: "none"|"image"|"video", url?: string}>}
  */
 export async function getCustomBackgroundData() {
     const dataUrl = (await chrome.storage.local.get())["secretSettings_customBackground"]
+    return getCustomBackgroundDataFromURL(dataUrl)
+}
+
+/**
+ * Gets the blob: URL associated with the custom background URL and its content type. This uses a web worker to avoid blocking the main thread.
+ * @param {string|URL} dataUrl URL that should be fetched
+ * @returns {Promise<{type: "none"|"image"|"video", url?: string}>}
+ */
+export async function getCustomBackgroundDataFromURL(dataUrl) {
     return new Promise((resolve, reject) => {
         fetchWorker.postMessage(dataUrl)
         fetchWorker.addEventListener("message", (event) => {
